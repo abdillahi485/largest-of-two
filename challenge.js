b2fcaa87f4802a27ae42f7a504218a596e7bd16f
Abdillahi485 <daakircadde143@gmail.com>
2023-10-25 20:03:16 +0300

    The largest number

--- challenge.js
+++ challenge.js
@@ -1,2 +1,9 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+
+let firstNumber=100;
+let secondNumber=200;
+
+if (secondNumber > firstNumber){
+    console.log('The largest number out of the two numbers is 200')
+}
